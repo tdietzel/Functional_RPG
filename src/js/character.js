4c973b02569afd88bd123e17df1237000ab46b1d
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-30 08:52:47 -0800

    Updated using pure functional programming
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Aaron Mejia <mejia-dev@users.noreply.github.com>

--- src/js/character.js
+++ src/js/character.js
@@ -1,25 +1,21 @@
-export const changeState = (prop) => {
-  return (value) => {
-    return (state) => (
-      {
-        ...state,
-        [prop]: (state[prop] || 0) + value
-      }
-    )
+export const changeState = (prop) => (value) => (state) => (
+  {
+    ...state,
+    [prop]: (state[prop] || 0) + value
   }
-}
+);
 
 // not tested, test later
-export const updateState = (prop) => {
-  return (value) => {
-    return (state) => (
-      {
-        ...state,
-        [prop]: (state[prop] || 0) = value
-      }
-    )
-  }
-}
+// export const updateState = (prop) => {
+//   return (value) => {
+//     return (state) => (
+//       {
+//         ...state,
+//         [prop]: (state[prop] || 0) = value
+//       }
+//     )
+//   }
+// }
 
 export const storeState = () => {
   let currentState = {};
@@ -36,30 +32,22 @@ export const character = (inputName) => {
   }
 }
 
-export const characterSelect = (state) => {
-  return function (type) {
-    if (type === "caveman") {
-      return {
-        ...state,
-        health: 10,
-        strength: 8,
-        intelligence: 2
-      }
-    }
-    else if (type === "cavedog") {
-      return {
-        ...state,
-        health: 5,
-        strength: 4, 
-        intelligence: 10
-      }
-    } else {
-      return {
-        ...state,
-        health: 2,
-        strength: 2, 
-        intelligence: 0
-      }
-    }
+export const characterSelect = (state) => (type) => {
+  return type === "caveman" ? {
+    ...state,
+    health: 10,
+    strength: 8,
+    intelligence: 2
+  } :
+  type === "cavedog" ? {
+    ...state,
+    health: 5,
+    strength: 4, 
+    intelligence: 10
+  } : {
+    ...state,
+    health: 2,
+    strength: 2, 
+    intelligence: 0
   }
 }
